ec3f79b3bb61d0c180a27c1ab8b8aee4cce613e9
turalbabayev <turalbabayev@outlook.com.tr>
2024-12-12 14:05:48 +0300

    Initial Commit

---
+++ Kultura/App/KulturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KulturaApp.swift
+//  Kultura
+//
+//  Created by Tural Babayev on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct KulturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
