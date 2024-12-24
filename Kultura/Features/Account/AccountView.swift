ce3c66c0972498333a4c204c9f36db79cc107bc9
turalbabayev <turalbabayev@outlook.com.tr>
2024-12-24 23:28:00 +0300

    update v1.1.5

---
+++ Kultura/Features/Account/AccountView.swift
@@ -0,0 +1,16 @@
+//
+//  AccountView.swift
+//  Kultura
+//
+//  Created by Tural Babayev on 24.12.2024.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
