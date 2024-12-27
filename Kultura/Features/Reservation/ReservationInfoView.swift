422c25ea954fd666c30ce80390fc030ffaded823
turalbabayev <turalbabayev@outlook.com.tr>
2024-12-27 10:37:01 +0300

    update v.1.2.5 - reservation info view static complated

--- Kultura/Features/Reservation/ReservationInfoView.swift
+++ Kultura/Features/Reservation/ReservationInfoView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ReservationInfoView: View {
     @State private var phoneNumber: String = ""
     @State private var selectedMeeting: String = "Business Lunch"
-    @State private var meetingOption = ["Business lunch", "Team meeting", "Project review", "Casual catch-up"]
+    @State private var meetingOptions = ["Business lunch", "Team meeting", "Project review", "Casual catch-up"]
+    
+    var fullPhoneNumber: String {
+        "+994" + phoneNumber
+    }
 
     @Environment(\.dismiss) var dismiss
 
@@ -18,22 +22,89 @@ struct ReservationInfoView: View {
         ZStack {
             NavigationView {
                 VStack(alignment: .leading, spacing: 16) {
-                    Text("Merhaba sayfa")
-                        .font(.headline)
+                    Text("Your Info")
+                        .font(AppFonts.customFont(name: "Poppins", size: 18))
+                        .padding(.horizontal)
 
-                    NavigationLink(destination: Text("Detay Sayfası")) {
-                        Text("Detaylara Git")
-                            .font(.body)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(8)
+                    // Phone Number Input
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Phone number")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                        
+                        HStack {
+                            Text("+994 | ")
+                                .foregroundColor(.gray)
+                                .padding(.leading, 8)
+                                .font(AppFonts.customFont(name: "Poppins", size: 14))
+                            
+                            TextField("", text: $phoneNumber)
+                                .keyboardType(.numberPad)
+                                .padding(20)
+                                .background(Color(UIColor.systemGray5))
+                                .cornerRadius(24)
+                                .onChange(of: phoneNumber) { oldValue, newValue in
+                                    // Kullanıcı sadece rakam girebilsin
+                                    phoneNumber = newValue.filter { $0.isNumber }
+                                }
+                        }
+                        .background(Color(UIColor.systemGray5))
+                        .cornerRadius(16)
                     }
+                    .padding(.horizontal)
+                    
+                    // Meeting Selection
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Görüşün adı")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                        
+                        Menu {
+                            ForEach(meetingOptions, id: \.self) { option in
+                                Button(action: {
+                                    selectedMeeting = option
+                                }) {
+                                    Text(option)
+                                }
+                            }
+                        } label: {
+                            HStack {
+                                Text(selectedMeeting)
+                                    .foregroundColor(.black)
+                                    .font(AppFonts.customFont(name: "Poppins", size: 14))
+                                
+                                Image(systemName: "chevron.down")
+                                    .foregroundColor(.gray)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding()
+                            .background(Color(UIColor.systemGray5))
+                            .cornerRadius(16)
+                        }
+                    }
+                    .padding(.horizontal)
+
+                    
 
                     Spacer()
+                    
+                    HStack(alignment: .bottom){
+                        NavigationLink(destination: TicketView()) {
+                            Text("Continue")
+                                .font(AppFonts.customFont(name: "Poppins-Regular", size: 16))
+                                .frame(maxWidth: 150) // Buton genişliği
+                                .padding()
+                                .background(Color(UIColor.systemGray4))
+                                .cornerRadius(20)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
                 }
+                //.padding()
                 .frame(maxWidth: .infinity)
-                .padding(.top, 16)
                 .background(Color("appSecondary")) // İçerik için arka plan rengi
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
@@ -50,6 +121,8 @@ struct ReservationInfoView: View {
                             .font(AppFonts.customFont(name: "Poppins", size: 16))
                     }
                 }
+                
+                
             }
             .background(Color.clear) // NavigationView'in arka planını şeffaf yap
         }
